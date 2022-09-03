391d8f140da6d0c80e08e8b95a68f51b19b83e07
DijkstraPhoenix <109066118+DijkstraPhoenix@users.noreply.github.com>
2022-09-03 19:15:39 +0800

    Create HdFl.h

---
+++ Cpp/INCLUDE/HdFl.h
@@ -0,0 +1,16 @@
+/*
+
+# Cpp - Header File
+
+# FIX-CPT
+
+*/
+
+#pragma GCC optimize("Ofast")
+#include<iostream>
+#include<cstdio>
+#include<cstdlib>
+#include<windows.h>
+#include<ctime>
+
+using namespace std;
